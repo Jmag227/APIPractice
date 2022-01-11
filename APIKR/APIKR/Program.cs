4174c7193232322e3113099ebb7a7a408ece52b1
Jmag227 <95197785+Jmag227@users.noreply.github.com>
2022-01-11 12:54:41 -0700

    init

---
+++ APIKR/APIKR/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace APIKR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+                Console.WriteLine();
+                QuoteGenerator.SwansonQuote();
+                Console.WriteLine();
+            }
+        }
+            
+    }
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = " https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: '{kanyeQuote}'");
+        }
+
+        public static void SwansonQuote()
+        {
+            var client = new HttpClient();
+
+            var swanURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var swanResponse = client.GetStringAsync(swanURL).Result;
+
+            var swanQuote = JArray.Parse(swanResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron: {swanQuote}");
+        }
+
+    }
+}
---
+++ OpenWeather/OpenWeather/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Please enter your API Key:");
+            var api_Key = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter city name: ");
+                var cityName = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={api_Key}";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+                Console.WriteLine("Would you like to choose anohter city?");
+                var userinput = Console.ReadLine();
+                Console.WriteLine();
+
+                if (userinput.ToLower() == "no")
+                {
+                    break;
+                }
+            }
+            
+        }
+    }
+}
